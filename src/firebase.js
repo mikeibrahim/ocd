56e279a8c34c47bcd69c133f750c81712ad67b21
Michael Ibrahim <mike.a.ibrahim@gmail.com>
2022-06-07 17:44:26 -0700

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXRfsFs454fS-hwBrbsGeUbDCJDTyCvC4",
+  authDomain: "obsessive-cubing-disorder.firebaseapp.com",
+  projectId: "obsessive-cubing-disorder",
+  storageBucket: "obsessive-cubing-disorder.appspot.com",
+  messagingSenderId: "829537686858",
+  appId: "1:829537686858:web:5cf2d6231d059b7cb25db3",
+  measurementId: "G-RZ14HT5NB9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
